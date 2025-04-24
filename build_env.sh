5da3e1705ddca336f2df728c6a7e3bf2238a0f3b
Zhifan Ye <yezhifan@mail.ustc.edu.cn>
2025-04-24 06:27:45 -0400

    [add]: edit env requirements

--- build_env.sh
+++ build_env.sh
@@ -4,17 +4,23 @@
 export CUDA_HOME="/usr/local/cuda-12.4"
 nvcc -V
 
+conda create -n longmamba python=3.10
+conda activate longmamba
+
 # Install PyTorch with the specified CUDA version
 pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cu124
 
 # Install other packages
-pip install vllm==0.5.3.post1
 pip install nltk
 pip install --upgrade transformers
 pip install tiktoken
 pip install sentencepiece
 pip install protobuf
 pip install ninja einops triton packaging
+pip install tabulate
+pip install jieba
+pip install fuzzywuzzy
+pip install rouge
 
 # install Mamba
 pip install -e .
